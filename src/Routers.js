e95e227105b978da3fed00157facaf92046e1da0
victor mann <victorcmann@gmail.com>
2020-05-17 10:27:46 -0300

    component SignUp

--- src/Routers.js
+++ src/Routers.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 export default () => {
     return (
@@ -16,6 +17,9 @@ export default () => {
             </Route>
             <Route exact path="/signin">
                 <SignIn />
+            </Route>
+            <Route exact path="/signup">
+                <SignUp />
             </Route>
             <Route>
                 <NotFound />
