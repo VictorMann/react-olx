5a7bd18af6f771e50e2c7625d3ed3f3c2e21cbd6
victor mann <victorcmann@gmail.com>
2020-05-23 15:39:58 -0300

    adPage

--- src/pages/AdPage/index.js
+++ src/pages/AdPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { PageContainer } from '../../components/MainComponents';
 import { PageArae } from './styled';
@@ -10,9 +10,29 @@ const Page = () => {
 
     const api = useApi();
     const [loading, setLoading] = useState(true);
-    const [adInfo, setAdInfo] = useState([]);
+    const [adInfo, setAdInfo] = useState({});
 
     const { id } = useParams();
+
+    useEffect(() => {
+        const getAdInfo = async (id) => {
+            const json = await api.getAd(id, true);
+            setAdInfo(json);
+            setLoading(false);
+        };
+        getAdInfo(id);
+    }, []);
+
+
+    const formatDate = (date) => {
+        let cDate = new Date(date);
+
+        let months = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'];
+        let cDay = cDate.getDate();
+        let cMonth = cDate.getMonth();
+        let cYear = cDate.getFullYear();
+        return `${cDay} de ${months[cMonth]} de ${cYear}`;
+    };
 
     return (
         <PageContainer>
@@ -25,9 +45,20 @@ const Page = () => {
                         <div className="adInfo">
                             <div className="adName">
                                 {loading && <Fake height={20} />}
+                                {adInfo.title &&
+                                    <h2>{adInfo.title}</h2>
+                                }
+                                {adInfo.dateCreated &&
+                                    <small>Criado em {formatDate(adInfo.dateCreated)}</small>
+                                }
                             </div>
                             <div className="adDescription">
                                 {loading && <Fake height={100} />}
+                                {adInfo.description}
+                                <hr />
+                                {adInfo.views &&
+                                    <small>Visualizações: {adInfo.views}</small>
+                                }
                             </div>
                         </div>
                     </div>
