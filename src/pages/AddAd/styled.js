12eda51373283a25c461cf774ef7d965e0ded259
victor mann <victorcmann@gmail.com>
2020-05-25 10:36:25 -0300

    page AddAd

---
+++ src/pages/AddAd/styled.js
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+export const PageArae = styled.div`
+form {
+    background: white;
+    border-radius: 3px;
+    padding: 10px;
+    box-shadow: 0 0 3px #999;
+
+    .area {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        max-width: 500px;
+
+        .area--title {
+            width: 200px;
+            text-align: right;
+            padding-right: 20px;
+            font-weight: bold;
+            font-size: 14px;
+        }
+        .area--input {
+            flex: 1;
+
+            input, select, textarea {
+                width: 100%;
+                font-size: 14px;
+                padding: 5px;
+                border: 1px solid #ddd;
+                border-radius: 3px;
+                outline: 0;
+                transition: .4s;
+
+                &:focus {
+                    border-color: #333;
+                    color: #333;
+                }
+            }
+
+            textarea {
+                height: 150px;
+                resize: none;
+            }
+
+            button {
+                background: #0089ff;
+                border: 0;
+                outline: 0;
+                padding: 5px 10px;
+                border-radius: 4px;
+                color: #fff;
+                font-size: 15px;
+                cursor: pointer;
+
+                &:hover {
+                    background: #006fce;
+                }
+            }
+        }
+    }
+}
+`;
